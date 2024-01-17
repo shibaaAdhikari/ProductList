11956fde0d8d52d24516d85fe7afc6be7ca7f6ff
shibaaAdhikari <shibaaadhikari0@gmail.com>
2024-01-17 20:38:14 +0545

    added product category

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,11 +1,19 @@
-import React from 'react'
-
-const navbar = () => {
-  return (
-    <div className='h-20 w-100 bg-slate-400'>
-     <p className='text-white'>logo</p>
-    </div>
-  )
-}
-
-export default navbar
+// components/Navbar.js
+const Navbar = () => {
+    return (
+      <nav className="bg-customColor">
+        <div className="container mx-auto flex justify-between items-center bg-vintage p-5 text-white cursor-pointer">
+          <div className="mr-4">
+            ProductList
+          </div>
+          <div className="flex items-center">
+            <h1 className="mr-4">Username</h1>
+            <h1>Logout</h1>
+          </div>
+        </div>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
+  
